add55571d1b7ef88c6da59da68a609e6cd21e254
modcomlearning <modcomlearning@gmail.com>
2026-05-29 15:12:10 +0300

    Final

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -94,7 +94,7 @@ export default function Login() {
         </div>
 
         <p style={{ textAlign: "center", marginTop: 20, fontSize: 13, color: "var(--text3)" }}>
-          <code style={{ color: "var(--accent2)" }}>Copyright &copy; 2026. MODCOM LTD</code>
+          <code style={{ color: "var(--accent2)" }}>Copyright &copy; 2026.</code>  MODCOM LTD
         </p>
       </div>
     </div>
